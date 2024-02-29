db57aa1ae6a09b2de1e2520d389624a7c40ee6e5
Eldin Guzin <pelda541@gmail.com>
2024-02-29 21:19:34 +0100

    Added 'add' method implementation

---
+++ homework1/untitled/src/main/java/org/example/DoubleNode.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class DoubleNode<Data> {
+    Data data;
+    DoubleNode<Data> next;
+    DoubleNode<Data> prev;
+}
---
+++ homework1/untitled/src/main/java/org/example/Node.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Node <Data>{
+    Data data;
+    Node<Data> next;
+}
