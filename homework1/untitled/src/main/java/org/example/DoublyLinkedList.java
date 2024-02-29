1cea0ed36f2f44fd281a3d495a4ea904e95e70a5
Eldin Guzin <pelda541@gmail.com>
2024-02-29 22:57:39 +0100

    added implementation for the rest of the methods

--- homework1/untitled/src/main/java/org/example/DoublyLinkedList.java
+++ homework1/untitled/src/main/java/org/example/DoublyLinkedList.java
@@ -1,5 +1,6 @@
 package org.example;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DoublyLinkedList<Data> implements Iterable<Data> {
     private DoubleNode<Data> head;
@@ -27,64 +28,147 @@ public class DoublyLinkedList<Data> implements Iterable<Data> {
 
     /* Remove a node from the front of the doubly linked list */
     public void removeFromFront() {
-        // your code
+        if (!isEmpty()) {
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head.next.prev = null;
+                head = head.next;
+            }
+            size--;
+        }
     }
+
 
     /* Add a new node to the end of the doubly linked list */
     public void addToRear(Data data) {
-        // your code
+        DoubleNode<Data> newNode = new DoubleNode<>();
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
+
 
     /* Remove a node at the end of the doubly linked list */
     public void removeFromRear() {
-        // your code
+        if (!isEmpty()) {
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                tail.prev.next = null;
+                tail = tail.prev;
+            }
+            size--;
+        }
     }
+
 
     /* Get a linked list node by index (0-indexed) */
     public Data get(int index) {
-        // your code
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        DoubleNode<Data> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.data;
     }
 
     /* Add an element to a doubly linked list by index (0-index) */
     public void add(int index, Data data) {
-        // your code
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (index == 0) {
+            addToFront(data);
+        } else if (index == size) {
+            addToRear(data);
+        } else {
+            DoubleNode<Data> newNode = new DoubleNode<>();
+            newNode.data = data;
+
+            DoubleNode<Data> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+
+            newNode.prev = current;
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            current.next = newNode;
+            size++;
+        }
     }
+
 
     /* Delete an element from a doubly linked list by index (0-index) */
     public void remove(int index) {
-        // your code
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (index == 0) {
+            removeFromFront();
+        } else if (index == size - 1) {
+            removeFromRear();
+        } else {
+            DoubleNode<Data> current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+
+            current.prev.next = current.next; // Update the next reference of the previous node
+            current.next.prev = current.prev; // Update the previous reference of the next node
+            size--;
+        }
     }
+
 
     /* Return the current size of the doubly linked list */
     public int count() {
-        //your code
-        return 0;
+        return size;
     }
+
 
     /* Return an Iterator Object */
     @Override
     public Iterator<Data> iterator() {
-        //your code
-        return null;
+        return new DoublyLinkedListIterator();
     }
+
 
     /* Define the Iterator class, and hasNext() and next() methods */
     private class DoublyLinkedListIterator implements Iterator<Data> {
-        //your code
+        private DoubleNode<Data> current = head;
 
         @Override
         public boolean hasNext() {
-            //your code
-            return false;
+            return current != null;
         }
 
         @Override
         public Data next() {
-            //your code
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException("No next element");
+            }
+            Data data = current.data;
+            current = current.next;
+            return data;
         }
     }
+
 
     /* Get head node (for test purposes) */
     public DoubleNode<Data> getHead() {
